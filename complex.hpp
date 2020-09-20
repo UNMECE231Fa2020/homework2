d4b55a417ac7823516d35ad6e19842bc9cb4e761
Fviramontes8 <kuolemajarvi@yahoo.com>
2020-09-20 17:28:28 -0600

    Added complex.hpp, the header file that will be used to create the Complex class

---
+++ complex.hpp
@@ -0,0 +1,37 @@
+class Complex {
+	private:
+		// Feel free to edit _real, _imag to whatever variable you want
+		double _real;
+		double _imag;
+
+	public:
+		Complex();
+
+		// Feel free to edit real, imag to whatever variable you want
+		Complex(double real, double imag);
+
+		// Feel free to edit c to whatever variable you want
+		Complex(const Complex &c);
+
+		// Getter declarations
+		double real() const;
+		double imag() const;
+
+		void print();
+
+		Complex sub(const Complex &c1, const Complex &c2);
+		Complex mult(const Complex &c1, const Complex &c2);
+
+		Complex conj();
+
+		double mag();
+		double phase();
+
+		Complex operator+(const Complex &c);
+		Complex operator/(const Complex &c);
+
+		Complex operator=(const Complex &c);
+
+		friend std::ostream &operator<<(std::ostream &out, const Complex &c);
+		friend std::istream &operator>>(std::istream &in, Complex &c);
+};
